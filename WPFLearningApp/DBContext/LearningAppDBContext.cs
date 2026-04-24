8c3700e76d51ccdb297d747d27f3a68ac76badb5
LandonKWalls <LandonKWalls@gmail.com>
2026-04-23 19:56:30 -0500

    DB context
    hit a bit of a roadblock and can't follow the tutorial anymore. Tutor started using Microsoft.EntityFrameworkCore, which is incompatible with .NetFramework v4.7.2. Will have to figure out how to use EntityFramework 6 for this project.
    
    https://www.youtube.com/watch?v=STt3U122wiU&list=PLA8ZIAm2I03hS41Fy4vFpRw8AdYNBXmNm&index=6

---
+++ WPFLearningApp/DBContext/LearningAppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFLearningApp.Models.Window2;
+
+namespace WPFLearningApp.DBContext
+{
+    class LearningAppDBContext : DbContext
+    {
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
---
+++ WPFLearningApp/DTO/ReservationDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLearningApp.DTO
+{
+    class ReservationDTO
+    {
+        [Key] public Guid ID;
+        public int FloorNumber;
+        public int RoomNumber;
+        public string Username;
+        public DateTime StartTime;
+        public DateTime EndTime;
+    }
+}
